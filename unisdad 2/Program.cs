b70761e9d9c45e77b81d56a8250a1c41dda682fb
CinthyaRM <146391744+CinthyaRM@users.noreply.github.com>
2023-09-29 16:57:57 -0400

    mis primeros programas

---
+++ unisdad 2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //DECLARAR VARIABLES
+            String nombre = "Roberto Ochoa";
+            //Imprimir nombre referenciado a una variable
+            Console.WriteLine(nombre);
+                //Cada linea tiene que terminar con ; punto y coma
+        }
+    }
+}
